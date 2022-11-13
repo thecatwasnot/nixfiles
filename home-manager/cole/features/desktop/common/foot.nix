00f5977fb483317ab9181e253f195b3e23ccb222
Cole Teeter <cole.teeter@gmail.com>
2022-11-12 18:38:02 -0600

    [cole] fix: Fix foot.ini file typo.

--- home-manager/cole/features/desktop/common/foot.nix
+++ home-manager/cole/features/desktop/common/foot.nix
@@ -1,6 +1,6 @@
 { pkgs, config, ... }: {
   home.packages = [ pkgs.foot ];
-  home.file.".config/food/foot.ini".text = ''
+  home.file.".config/foot/foot.ini".text = ''
     [main]
     font = ${config.fontProfiles.monospace.family}
   '';
