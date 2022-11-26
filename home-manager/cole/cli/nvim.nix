529eaac87921acc15b946f053206f228984f8c7a
Cole Teeter <cole.teeter@gmail.com>
2022-11-25 19:51:20 -0600

    [cole/cli/nvim] fix: Change telescope to use lua config.
    It now uses the proper leader config.

--- home-manager/cole/cli/nvim.nix
+++ home-manager/cole/cli/nvim.nix
@@ -12,12 +12,14 @@
     [ 
       {
         plugin =  telescope-nvim;
+        type = "lua";
         config = ''
-					nnoremap <leader>ff <cmd>Telescope find_files<cr>
-					nnoremap <leader>fg <cmd>Telescope live_grep<cr>
-					nnoremap <leader>fb <cmd>Telescope buffers<cr>
-					nnoremap <leader>fh <cmd>Telescope help_tags<cr>
-				'';
+          local builtin = require('telescope.builtin')
+          vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+          vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+          vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+          vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+        '';
       }
 
       {
