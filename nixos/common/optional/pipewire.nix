1736f62c02a7b175272da4e6b719b78a04a7b6e5
Cole Teeter <cole.teeter@gmail.com>
2022-12-09 20:55:37 -0600

    [pipewire] fix: Better configuration for pipewire-alsa

--- nixos/common/optional/pipewire.nix
+++ nixos/common/optional/pipewire.nix
@@ -6,4 +6,25 @@
     pulse.enable = true;
     wireplumber.enable = true;
   };
+  environment.etc."wireplumber/main.lua.d/51-pulsarcustom.lua" = {
+    text = ''
+          alsa_monitor.rules = {
+        {
+          matches = {
+            {
+              -- Matches all sources.
+              { "node.name", "matches", "alsa_input.*" },
+            },
+            {
+              -- Matches all sinks.
+              { "node.name", "matches", "alsa_output.*" },
+            },
+          },
+          apply_properties = {
+            ["api.alsa.headroom"]      = 64,
+          },
+        },
+      }
+    '';
+  };
 }
