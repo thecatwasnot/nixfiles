ca02e44fe02dfca78575dadcffa47fb9a5eca5b4
Cole Teeter <cole.teeter@gmail.com>
2024-03-22 09:22:58 -0500

    Initial commit: Snowfall-lib flake.  We're all made of stardust.

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Cole's Modern Nix Config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    snowfall-lib = {
+      url = "github:snowfallorg/lib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+  };
+
+  outputs = inputs:
+    inputs.snowfall-lib.mkFlake {
+      inherit inputs;
+      src = ./.;
+
+      snowfall = {
+        meta = {
+          name = "stardust";
+          title = "StarDust";
+        };
+        namespace = "stardust";
+      };
+    };
+}
