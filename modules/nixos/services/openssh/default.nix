ad2ac3531fb0dd8f19991a91e7f9ae8578411315
Cole <cole.teeter@gmail.com>
2025-01-13 03:55:36 -0600

    Remove machine-id persistance from ssh to move it to the general impermanence config

--- modules/nixos/services/openssh/default.nix
+++ modules/nixos/services/openssh/default.nix
@@ -30,7 +30,6 @@ in
         "/root/ssh"
       ];
       files = [
-        "/etc/machine-id"
         "/etc/ssh/ssh_host_ed25519_key"
         "/etc/ssh/ssh_host_ed25519_key.pub"
         "/etc/ssh/ssh_host_rsa_key"
