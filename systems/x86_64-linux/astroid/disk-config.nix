d88a2ba37c218c6c480833e70a593c0ab6561a6e
Cole Teeter <cole.teeter@gmail.com>
2025-04-08 17:16:54 -0500

    feat: Add first pass, basics of zm server.

--- systems/x86_64-linux/astroid/disk-config.nix
+++ systems/x86_64-linux/astroid/disk-config.nix
@@ -19,7 +19,9 @@
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
+                mountOptions = [
+                  "umask=0077"
+                ];
               };
             };
             crypt = {
