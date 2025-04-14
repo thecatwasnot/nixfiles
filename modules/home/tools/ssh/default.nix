b94133a2ad82cda94fb2e886c991317138dd2660
Cole Teeter <cole.teeter@gmail.com>
2025-04-14 15:06:21 -0500

    broken: Client side ssh config for gpg-agent forward
    This adds part of the client side configuration.
    We need ip or real addresses configured.
    SSH doesn't know we've connected to one of our own hosts.

--- modules/home/tools/ssh/default.nix
+++ modules/home/tools/ssh/default.nix
@@ -2,12 +2,15 @@
   config,
   lib,
   namespace,
+  systems,
   ...
 }:
 with lib;
 with lib.${namespace};
 let
   cfg = config.${namespace}.tools.ssh;
+  
+  hostnames = lib.unique ( builtins.attrNames systems );
 in
 {
   options.${namespace}.tools.ssh = {
@@ -17,6 +20,20 @@ in
   config = mkIf cfg.enable {
     programs.ssh = {
       enable = true;
+      matchBlocks = {
+        "${namespace}" = {
+          host = lib.concatStringsSep " " ( lib.flatten (map (host: [
+             host
+           ])
+           hostnames));
+          remoteForwards = [
+            {
+              bind.address = ''/%d/.gnupg-sockets/S.gpg-agent'';
+              host.address = ''/%d/.gnupg-sockets/S.gpg-agent.extra'';
+            }
+          ];
+        };
+      };
     };
   };
 }
